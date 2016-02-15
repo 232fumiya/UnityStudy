7fe74750da9a937d5a48ef7e962db547f9ce3f33
213fumiya <21311232fn@tama.ac.jp>
2016-02-16 03:31:36 +0900

    エディター拡張テスト

---
+++ Editor/checker.cs
@@ -0,0 +1,25 @@
+﻿//C# Example
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+class checker : EditorWindow {
+	[MenuItem ("Window/checker")]	
+	public static void  ShowWindow () {
+		EditorWindow.GetWindow(typeof(checker));
+	}
+	
+	void OnGUI () {
+		// The actual window code goes here
+		int qualityLevel = QualitySettings.GetQualityLevel();
+		int vSync = QualitySettings.vSyncCount;
+		string isVSync="YES";
+		if (vSync == 0) {
+			isVSync="NO";
+		}
+		GUILayout.Label ("FPSchecker", EditorStyles.boldLabel);
+		GUILayout.Label("vSync="+isVSync);
+		GUILayout.Label ("targetFPS=" + Application.targetFrameRate);
+		GUILayout.Label("QualityLevel="+qualityLevel.ToString ());
+	}
+}
