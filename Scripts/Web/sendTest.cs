33af20be02c19f0aa560d24b9bc2efea64b80cf4
213fumiya <21311232fn@tama.ac.jp>
2016-02-20 16:46:53 +0900

    add sendTest.cs

---
+++ Scripts/Web/sendTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Send test.
+/// formでデータを投げる
+/// </summary>
+public class sendTest : MonoBehaviour {
+	private string message="message";
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("sendDataTest");
+	}
+
+	private IEnumerator sendDataTest()
+	{
+		string URL = "データを飛ばすurl";
+		WWWForm form = new WWWForm ();
+		form.AddField("test",message);
+		WWW www = new WWW (URL,form);
+		yield return www;
+		if (www.error != null)
+			Debug.Log (www.error);
+		else
+			Debug.Log("success!");
+	}
+}
