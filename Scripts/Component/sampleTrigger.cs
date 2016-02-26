b2108a0d108e646523164f1c6162608d5a85798f
213fumiya <21311232fn@tama.ac.jp>
2016-02-26 15:44:08 +0900

    add sampleTrigger.cs

---
+++ Scripts/Component/sampleTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sampleTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("enter:"+other.name);
+	}
+	void OnTriggerStay(Collider other)
+	{
+		Debug.Log ("stay:"+other.name);
+	}
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log ("exit:"+other.name);
+	}
+}
