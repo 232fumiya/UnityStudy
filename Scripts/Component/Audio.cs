42c43d5693b85c4ac173870489e2c917654f75bd
213fumiya <21311232fn@tama.ac.jp>
2016-03-01 23:23:42 +0900

    add Audio.cs

---
+++ Scripts/Component/Audio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour {
+	AudioSource sample;
+	// Use this for initialization
+	void Start () {
+		sample = this.GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (!sample.isPlaying&&Input.GetKeyDown(KeyCode.A))
+			sample.Play ();
+	}
+}
