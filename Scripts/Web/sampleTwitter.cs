382645ea3c2bf3dd248ac5c6048c5e2ed0e69198
213fumiya <21311232fn@tama.ac.jp>
2016-03-02 17:23:08 +0900

    add sampleTwitter.cs

---
+++ Scripts/Web/sampleTwitter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sampleTwitter : MonoBehaviour {
+	bool isTweeting=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A))
+			StartCoroutine ("tweet");
+	}
+	IEnumerator tweet()
+	{
+		if (isTweeting)
+			yield break;
+		else
+			isTweeting = true;
+		Application.OpenURL("http://twitter.com/intent/tweet?text=" + WWW.EscapeURL("テキスト #hashtag"));
+		yield return null;
+		isTweeting = false;
+	}
+}
