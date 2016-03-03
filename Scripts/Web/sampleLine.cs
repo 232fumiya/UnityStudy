8c6f62edf9c372ca57d032d8c73c59e7f0a46185
213fumiya <21311232fn@tama.ac.jp>
2016-03-03 15:03:28 +0900

    add sampleLine.cs

---
+++ Scripts/Web/sampleLine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sampleLine : MonoBehaviour {
+	bool isSending=false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A))
+			StartCoroutine ("send");
+	}
+	IEnumerator send()
+	{
+		if (isSending)
+			yield break;
+		else
+			isSending = true;
+		string message = "Unityから投稿するテスト";
+		Application.OpenURL("http://line.naver.jp/R/msg/text/?" + WWW.EscapeURL(message, System.Text.Encoding.UTF8));yield return null;
+		isSending = false;
+	}
+}
