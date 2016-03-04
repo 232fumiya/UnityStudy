3f9171bf5f9b5c2d4c9cd1678e74a7de3e6f00a2
213fumiya <21311232fn@tama.ac.jp>
2016-03-04 16:14:10 +0900

    add ButtonSample.cs

---
+++ Scripts/Component/UI/ButtonSample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ButtonSample : MonoBehaviour {
+
+	private Button button1;
+	// Use this for initialization
+	void Start () {
+		button1=this.GetComponent<Button> ();
+		button1.onClick.AddListener (testClickListener);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	void testClickListener()
+	{
+
+	}
+}
