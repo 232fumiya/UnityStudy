26a3dd54787f86330460555130f4b9e04d862cdc
213fumiya <21311232fn@tama.ac.jp>
2016-03-05 22:12:11 +0900

    add LifeGage.cs

---
+++ Scripts/Component/UI/LifeGage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class LifeGage : MonoBehaviour {
+	private Image image1;
+	public int startLife=10;
+	public static int life;
+	// Use this for initialization
+	void Start () {
+		image1=this.GetComponent<Image> ();
+		life=startLife;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//lifeが減ると、ゲージが縮む
+		image1.fillAmount = life / startLife;
+	}
+}
