e67f7e3bba2294bd2c56c2dd7470738d4f146e6f
213fumiya <21311232fn@tama.ac.jp>
2016-03-06 23:33:11 +0900

    add TextSample.cs

---
+++ Scripts/Component/UI/TextSample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TextSample : MonoBehaviour {
+	private Text tex;
+	// Use this for initialization
+	void Start () {
+		tex = GameObject.Find ("Text").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tex.text="";
+	}
+}
